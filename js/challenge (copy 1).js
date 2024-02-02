e45c7f7dcfa087eca19221872660d0a187e50156
Beelzebu <saulices@gmail.com>
2024-02-02 16:30:38 -0500

    Index y JS Updated

--- js/challenge (copy 1).js
+++ js/challenge (copy 1).js
@@ -18,8 +18,15 @@ function challenge(event){
 }
 
 function mostrarElementos(){
-   document.getElementById('alura-boy').style.display = '';
+    let texto = document.getElementById('texto-encriptado');
+    let texto1 = document.getElementById('texto-original');
+    if(texto.value.trim() === ''){
+        document.getElementById('alura-boy').style.display = '';
         document.getElementById('mensaje-alerta').style.display = '';
         document.getElementById('alerta').style.display = '';
         document.getElementById('btn-copiar').style.display = 'none';
+      
+    }else{
+        document.getElementById('texto-encriptado').innerHTML = document.getElementById('texto-original').value;
+    }
 }
